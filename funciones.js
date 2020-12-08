76e8071db5bd390419971c872c3ee8aee8b457f9
LiliaAlmaraz <61852167+LiliaAlmaraz@users.noreply.github.com>
2020-12-07 18:37:44 -0600

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,24 @@
+window.onload = init;
+function init(){
+
+    document.querySelector('.etiquetas').addEventListener('click', function() {
+        window.location.href = "http://lilialmaraz.ml/ETIQUETAS/Etiquetas.html"
+    });
+
+    document.querySelector('.login').addEventListener('click', function() {
+        window.location.href = "http://lilialmaraz.ml/LOGIN/"
+    });
+
+    document.querySelector('.contenedores').addEventListener('click', function() {
+        window.location.href = "http://lilialmaraz.ml/CONTENEDORES/"
+    });
+
+    document.querySelector('.login2').addEventListener('click', function() {
+        window.location.href = "https://liliaalmaraz.github.io/Login_Instragram/"
+    });
+
+    document.querySelector('.calculadora').addEventListener('click', function() {
+        window.location.href = "https://liliaalmaraz.github.io/Calculadora/"
+    });
+
+}
